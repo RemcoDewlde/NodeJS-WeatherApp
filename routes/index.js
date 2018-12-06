dc54c748267286facd419021651e8466308fa6d4
Remco <dewilderemco13@gmail.com>
2018-12-06 20:29:36 +0100

    changed the UI a bit to comply with the prof.

--- routes/index.js
+++ routes/index.js
@@ -13,23 +13,32 @@ router.get('/weather', function (req, res, next) {
     let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=5d80bc283a8685b14fa7128658df8a0a&units=metric';
 
     request(url, function (error, response, body) {
-        let weather_json = JSON.parse(body);
 
-        if (weather_json.cod === '404') {
-            let error = weather_json.message;
-            res.redirect('/?error=' + error);
-            res.render('index', {error: weather_json.cod})
+        try {
+            let weather_json = JSON.parse(body);
 
-        } else {
-            let weather = {
-                temp: Math.round(weather_json.main.temp),
-                desc: weather_json.weather[0].description,
-                city: weather_json.name,
-                code: weather_json.weather[0].id,
-                wins: weather_json.wind.speed,
-            };
+            if (weather_json.cod === '404') {
+                let error = weather_json.message;
+                res.redirect('/?error=' + error);
+                res.render('index', {error: weather_json.cod})
+            } else {
 
-            res.render('weather', {weather: weather, title: 'WeatherApp'})
+                let kts = 1.9438445 * weather_json.wind.speed;
+                let weather = {
+                    temp: Math.round(weather_json.main.temp),
+                    desc: weather_json.weather[0].description,
+                    city: weather_json.name,
+                    code: weather_json.weather[0].id,
+                    ms: weather_json.wind.speed,
+                    knots: kts.toFixed(1)
+                };
+
+                res.render('weather', {weather: weather, title: 'WeatherApp'})
+            }
+        }
+        catch (error) {
+            console.log('[ERROR]: ' + error);
+            res.render('index', {error: 'Something went wrong!'})
         }
     })
 });
